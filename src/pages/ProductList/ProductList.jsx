9c1b01fc4e1deba26dbf68764ddc5b8e97d68775
Yonier Garcia <yoniermosquera55@gmail.com>
2025-04-05 03:41:18 -0500

    ruta vercel

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card } from "../../components/card/card";
+import { Card } from "../../components/card/Card";
 import { useFetch } from "../../hooks/useFetch";
 import { useNavigate } from "react-router-dom";
 import "./ProductList.css";
