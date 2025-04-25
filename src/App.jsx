e2fe8b41fcc17c525c8e2b125449405cc2670941
Yonier Garcia <yoniermosquera55@gmail.com>
2025-04-25 05:22:43 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import ProductList from "./pages/ProductList/ProductList";
 import ProductDetail from "./components/ProductDetail/ProductDetail";
 import Header from "./components/header/Header";
 import Home from "./pages/Home/Home";
+import { Footer } from "./components/footer/Footer";
 
 function App() {
   return (
@@ -27,6 +28,9 @@ function App() {
                 <Route path="/products" element={<ProductList />}></Route>
                 <Route path="/product/:id" element={<ProductDetail />}></Route>
               </Routes>
+            </div>
+            <div className="FooterLayout">
+              <Footer />
             </div>
           </BrowserRouter>
         </CartProvider>
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Footer.css"; // Importa el archivo CSS para estilos del footer
+
+import { FaGithubSquare } from "react-icons/fa";
+
+import { FaLinkedin } from "react-icons/fa";
+import { IoLocationSharp } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa6";
+
+export function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-info">
+        <div className="location footer-info-item">
+          <IoLocationSharp className="icon-footer" />
+          <p>Buenaventura, Colombia</p>
+        </div>
+        <div className="phone footer-info-item">
+          <FaPhone className="icon-footer" />
+          <p>(+57)00000000</p>
+        </div>
+      </div>
+      <div className="footer-about-company">
+        <h1>Vibras Store</h1>
+        <p>
+          Tu espacio para descubrir productos únicos a precios increíbles. 🌟
+          Nos enfocamos en ofrecerte calidad, estilo y variedad en un solo
+          lugar. Ya sea que busques lo último en tendencia, regalos originales o
+          simplemente darte un gusto, aquí encontrarás lo que necesitas con
+          ofertas irresistibles.
+        </p>
+        <div className="socials">
+          <div className="social">
+            <a
+              href="https://github.com/YonierGM"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithubSquare />
+            </a>
+          </div>
+          <div className="social">
+            <a
+              href="https://www.linkedin.com/in/yoniergm/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
