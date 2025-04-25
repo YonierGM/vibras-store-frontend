b7eff87ffb1d5a18dc3fe66257cc743b6998c81a
Yonier Garcia <yoniermosquera55@gmail.com>
2025-04-24 23:02:29 -0500

    out lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,32 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CartProvider } from "./components/cart/CartContext";
-import { Toaster } from "react-hot-toast";
-import "./App.css";
 
-// Asegúrate de que estos componentes tengan export default
-const ProductList = lazy(() => import("./pages/ProductList/ProductList"));
-const ProductDetail = lazy(() =>
-  import("./components/ProductDetail/ProductDetail")
-);
-const Header = lazy(() => import("./components/header/Header"));
-const Home = lazy(() => import("./pages/Home/Home"));
+import { Toaster } from "react-hot-toast";
+
+import "./App.css";
+import ProductList from "./pages/ProductList/ProductList";
+import ProductDetail from "./components/ProductDetail/ProductDetail";
+import Header from "./components/header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
-      <Toaster position="bottom-right" reverseOrder={false} />
+      <Toaster position="button-right" reverseOrder={false} />
       <section className="Layout">
         <CartProvider>
           <BrowserRouter>
             <div className="HeaderLayout">
-              {/* <Suspense fallback={<div>Cargando header...</div>}>
-                <Header />
-              </Suspense> */}
+              <Header />
             </div>
             <div className="MainLayout">
-              <Suspense fallback={<div>Cargando página...</div>}>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/products" element={<ProductList />} />
-                  <Route path="/product/:id" element={<ProductDetail />} />
-                </Routes>
-              </Suspense>
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/home" element={<Home />}></Route>
+                <Route path="/products" element={<ProductList />}></Route>
+                <Route path="/product/:id" element={<ProductDetail />}></Route>
+              </Routes>
             </div>
           </BrowserRouter>
         </CartProvider>
