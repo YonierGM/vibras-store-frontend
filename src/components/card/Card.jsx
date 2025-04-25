8edeb9ddaf518898390d6447ff672810367b34b6
Yonier Garcia <yoniermosquera55@gmail.com>
2025-04-25 04:08:58 -0500

    preload back

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { CardFooter } from "../CardFooter/CardFooter";
 import { useOptimizedImage } from "../../hooks/useCloudinary";
@@ -17,12 +16,6 @@ export function Card({
 }) {
   const navigate = useNavigate();
   const optimizedImage = useOptimizedImage(image);
-
-  useEffect(() => {
-    // Precarga la imagen
-    const img = new Image();
-    img.src = optimizedImage || image;
-  }, [optimizedImage, image]);
 
   return (
     <div className="Card" onClick={() => navigate(`/product/${id}`)}>
